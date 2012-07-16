51bcb087bec8bac4f04444c7e1ac080c0bd8cc64
Kore Nordmann <mail@kore-nordmann.de>
2012-07-16 16:45:15 +0200

    Use jQuery UI sortable instead of unmaintained code

--- src/htdocs/scripts/torii.js
+++ src/htdocs/scripts/torii.js
@@ -1,35 +1,29 @@
 $(document).ready(function() {
-    $( ".column" ).each( function( key, list ) {
-        DragDrop.makeListContainer( list );
+    $( ".column" ).sortable( {
+        scroll: false,
+        opacity: .7,
+        connectWith: ".column"
+    } );
 
-        list.onDragOver = function() {
-            $( this ).addClass( "drag-over" );
-        };
-
-        list.onDragOut = function() {
-            $( this ).removeClass( "drag-over" );
-        };
-
-        DragDrop.onDragFinished = function() {
-            var configuration = [],
-                column;
-            $( ".column" ).each( function( columnNr, columnElement ) {
-                column = [];
-                $( columnElement ).children( "li" ).each( function( moduleNr, moduleElement ) {
-                    column.push( moduleElement.id );
-                } );
-                configuration.push( column );
+    $( ".column" ).bind( "sortupdate", function(event, ui) {
+        var configuration = [],
+            column;
+        $( ".column" ).each( function( columnNr, columnElement ) {
+            column = [];
+            $( columnElement ).children( "li" ).each( function( moduleNr, moduleElement ) {
+                column.push( moduleElement.id );
             } );
+            configuration.push( column );
+        } );
 
-            $.post(
-                "/portal/resort",
-                {modules: configuration},
-                function ( data, textStatus, xhr ) {
-                    console.log( textStatus + ": " + data.ok );
-                },
-                "json"
-            );
-        };
+        $.post(
+            "/portal/resort",
+            {modules: configuration},
+            function ( data, textStatus, xhr ) {
+                console.log( textStatus + ": " + data.ok );
+            },
+            "json"
+        );
     } );
 } );
 
