a9ca7de2fe6666bd89c0f4581074ec1741603073
Kore Nordmann <mail@kore-nordmann.de>
2012-07-16 17:23:42 +0200

    Allow to remove elements through dragging
    (With confirmation)

--- src/htdocs/scripts/torii.js
+++ src/htdocs/scripts/torii.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
-    $( ".column" ).sortable( {
+    $( ".column, .trash" ).sortable( {
         scroll: false,
         opacity: .7,
-        connectWith: ".column"
+        connectWith: ".column, .trash",
+        cursor: "move"
     } );
 
-    $( ".column" ).bind( "sortover", function(event, ui) {
+    // Center (hiffen) trash element
+    $( ".trash" ).offset( {
+        left: $( document ).innerWidth() / 2 - 100 - $( ".trash" ).offset().left
+    } );
+
+    $( ".column" ).bind( "sortstart", function(event, ui) {
+        $( ".trash" ).fadeIn( 500 );
+    } );
+
+    $( ".column, .trash" ).bind( "sortover", function(event, ui) {
         $( this ).addClass( "drag-over" );
     } );
 
-    $( ".column" ).bind( "sortout", function(event, ui) {
+    $( ".column, .trash" ).bind( "sortout", function(event, ui) {
         $( this ).removeClass( "drag-over" );
     } );
 
-    $( ".column" ).bind( "sortstop", function(event, ui) {
+    $( ".trash" ).bind( "sortreceive", function(event, ui) {
+        if ( !confirm( "Really remove module?" ) ) {
+            $( ui.sender ).sortable( 'cancel' );
+        }
+    } );
+
+    $( ".column, .trash" ).bind( "sortstop", function(event, ui) {
+        $( ".trash" ).hide();
+        $( ".trash" ).empty();
+
         var configuration = [],
             column;
         $( ".column" ).each( function( columnNr, columnElement ) {
