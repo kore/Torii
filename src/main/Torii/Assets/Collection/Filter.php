3c67a4186a3da956a273a8de741dea3ee6156536
Kore Nordmann <mail@kore-nordmann.de>
2012-07-21 20:41:35 +0200

    Stubbed: Asset filtering classes

---
+++ src/main/Torii/Assets/Collection/Filter.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Torii
+ *
+ * @version $Revision$
+ */
+
+namespace Torii\Assets\Collection;
+
+use Torii\Assets\Collection;
+
+/**
+ * Collection filter base class
+ *
+ * Used to implement common filter helper functions
+ *
+ * @version $Revision$
+ */
+class Filter extends Collection
+{
+}
+
