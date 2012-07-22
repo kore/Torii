804c6d7b8ea65f4e8cddd1defad1d5d048b22997
Kore Nordmann <mail@kore-nordmann.de>
2012-07-22 20:33:43 +0200

    Tested: merge multiple feeds

--- test/Torii/Module/Feed/ModelTest.php
+++ test/Torii/Module/Feed/ModelTest.php
@@ -206,5 +206,26 @@ class ModelTest extends DatabaseTest
             $model->getUnread( 'module_1' )
         );
     }
+
+    /**
+     * @depends testAddEntry
+     */
+    public function testMergeModules()
+    {
+        $model = new Model( $this->getDbal() );
+        $model->addUrl( "module_1", "http://example.com/1" );
+        $model->addUrl( "module_1", "http://example.com/2" );
+
+        $model->addEntry( 1, 'http://example.com/1/1', 12345, 'Foo' );
+        $model->addEntry( 2, 'http://example.com/2/1', 12346, 'Foo' );
+
+        $this->assertEquals(
+            array(
+                new Struct\FeedEntry( 2, 'http://example.com/2/1', 'Foo' ),
+                new Struct\FeedEntry( 1, 'http://example.com/1/1', 'Foo' ),
+            ),
+            $model->getUnread( 'module_1' )
+        );
+    }
 }
 
