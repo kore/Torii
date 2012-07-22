49d990295c725eba3cccdd8dcd08b42d51564410
Kore Nordmann <mail@kore-nordmann.de>
2012-07-22 15:08:36 +0200

    Updated feed DB schema to include read information

--- src/schema/0002_feed_module.sql
+++ src/schema/0002_feed_module.sql
@@ -1,43 +1,53 @@
--- Table: Feed Module (f_m)
+-- Table: Feed Module (feed_m)
 DROP TABLE IF EXISTS `feed_module`;
 CREATE TABLE `feed_module` (
-  `f_m_id` VARCHAR(32) NOT NULL,
-  `u_id` INT NOT NULL,
-  `f_m_settings` TEXT NOT NULL,
+  `feed_m_id` VARCHAR(32) NOT NULL,
+  `feed_m_settings` TEXT NOT NULL,
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`f_m_id`),
-  KEY(`u_id`)
+  PRIMARY KEY (`feed_m_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
 -- Table: Feed Module Url relation table
-DROP TABLE IF EXISTS `f_m_u_rel`;
-CREATE TABLE `f_m_u_rel` (
-  `f_m_id` INT AUTO_INCREMENT NOT NULL,
-  `f_u_id` INT NOT NULL,
+DROP TABLE IF EXISTS `feed_m_u_rel`;
+CREATE TABLE `feed_m_u_rel` (
+  `feed_m_id` INT AUTO_INCREMENT NOT NULL,
+  `feed_u_id` INT NOT NULL,
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`f_m_id`, `f_u_id`)
+  PRIMARY KEY (`feed_m_id`, `feed_u_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
--- Table: Feed Urls (f_u)
+-- Table: Feed Urls (feed_u)
 DROP TABLE IF EXISTS `feed_url`;
 CREATE TABLE `feed_url` (
-  `f_u_id` INT AUTO_INCREMENT NOT NULL,
-  `f_u_url` VARCHAR(255) NOT NULL,
-  `f_u_update` BIGINT NOT NULL,
-  `f_u_status` INT NOT NULL,
+  `feed_u_id` INT AUTO_INCREMENT NOT NULL,
+  `feed_u_url` VARCHAR(255) NOT NULL,
+  `feed_u_update` BIGINT NOT NULL,
+  `feed_u_status` INT NOT NULL,
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`f_u_id`),
-  KEY(`f_u_update`)
+  PRIMARY KEY (`feed_u_id`),
+  KEY(`feed_u_update`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
--- Table: Feed Data (f_d)
+-- Table: Feed Data (feed_d)
 DROP TABLE IF EXISTS `feed_data`;
 CREATE TABLE `feed_data` (
-  `f_u_id` INT AUTO_INCREMENT NOT NULL,
-  `f_d_time` BIGINT NOT NULL,
-  `f_d_data` TEXT NOT NULL,
+  `feed_d_id` INT AUTO_INCREMENT NOT NULL,
+  `feed_u_id` INT NOT NULL,
+  `feed_d_time` BIGINT NOT NULL,
+  `feed_d_data` TEXT NOT NULL,
   `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`f_u_id`),
-  KEY(`f_d_time`)
+  PRIMARY KEY (`feed_d_id`),
+  KEY(`feed_d_time`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
+
+-- Table: Feed Module Data relation table
+DROP TABLE IF EXISTS `feed_m_d_rel`;
+CREATE TABLE `feed_m_d_rel` (
+  `feed_m_id` VARCHAR(32) NOT NULL,
+  `feed_d_id` INT NOT NULL,
+  `feed_m_d_read` INT DEFAULT 0,
+  `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`feed_m_id`, `feed_d_id`),
+  KEY (`feed_m_d_read`)
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
