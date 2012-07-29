20514f204b3c911b1291f96f6a0a71e923a46a0d
Kore Nordmann <mail@kore-nordmann.de>
2012-07-29 14:39:49 +0200

    Back to ZF2 Zend_Feed
    Even this involves cloning the full fucking ZF2, this feed reader
    actually works.

--- src/modules/Feed/php/Parser.php
+++ src/modules/Feed/php/Parser.php
@@ -42,11 +42,11 @@ class Parser
                 return $feed;
             }
 
-            $reader = new \SimplePie();
-            $reader->set_raw_data( $response->getContent() );
-            $reader->init();
+            $reader = \Zend\Feed\Reader\Reader::importString(
+                $response->getContent()
+            );
 
-            foreach ( $reader->get_items() as $entry )
+            foreach ( $reader as $entry )
             {
                 $feed->entries[] = $this->parseEntry( $entry );
             }
@@ -66,98 +66,28 @@ class Parser
     }
 
     /**
-     * Converts a single feed entry into something sensible
+     * Converts a single zend feed entry into something sensible
      *
-     * @param \SimplePie_Item $entry
+     * @param mixed $data
      * @return Struct\FeedEntry
      */
-    protected function parseEntry( \SimplePie_Item $data )
+    protected function parseEntry( $data )
     {
         $entry = new Struct\FeedEntry(
             null,
-            $data->get_link(),
+            $data->getLink(),
             null,
-            $this->decode( $data->get_title() )
+            $data->getTitle()
         );
 
-        $entry->date        = $data->get_date( 'U' ) ?: time();
-        $entry->description = $this->decode( $data->get_description() );
-        $entry->content     = $this->decode( $data->get_content() );
+        $entry->date        = $data->getDateModified() ?
+            $data->getDateModified()->format( 'U' ) :
+            time();
+
+        $entry->description = $data->getDescription();
+        $entry->content     = $data->getContent();
 
         return $entry;
-    }
-
-    /**
-     * Decode all HTML and XML entities in the string
-     *
-     * @param string $string
-     * @return string
-     */
-    protected function decode( $string )
-    {
-        $string = html_entity_decode( $string );
-
-        $string = preg_replace_callback(
-            '(&#(\\d+);)',
-            function ( $matches )
-            {
-                return Parser::codePointToUtf8( $matches[1] );
-            },
-            $string
-        );
-
-        $string = preg_replace_callback(
-            '(&#x([a-fA-F0-9]+);)',
-            function ( $matches )
-            {
-                return Parser::codePointToUtf8( hexdec( $matches[1] ) );
-            },
-            $string
-        );
-
-        return $string;
-    }
-
-    /**
-     * Convert unicode code point to UTF-8 character
-     *
-     * @param int $decimal
-     * @return string
-     */
-    public static function codePointToUtf8( $decimal )
-    {
-        switch ( true )
-        {
-            case $decimal <= 0x007f:
-                return chr( $decimal );
-
-            case $decimal <= 0x07ff:
-                return
-                    chr( 0xc0 | ( $decimal >> 6 ) ) .
-                    chr( 0x80 | ( $decimal & 0x003f ) );
-
-            case $decimal === 0xFEFF:
-                return '';
-
-            case $decimal >= 0xD800 && $decimal <= 0xDFFF:
-                throw new \RuntimeException( "Surrogates are not handled." );
-
-            case $decimal <= 0xffff:
-                return
-                    chr( 0xe0 | ( $decimal >> 12 ) ) .
-                    chr( 0x80 | ( ( $decimal >> 6 ) & 0x003f ) ) .
-                    chr( 0x80 | ( $decimal & 0x003f ) );
-
-            case $decimal <= 0x10ffff:
-                return
-                    chr( 0xf0 | ( $decimal >> 18 ) ) .
-                    chr( 0x80 | ( ( $decimal >> 12 ) & 0x3f ) ) .
-                    chr( 0x80 | ( ( $decimal >> 6 ) & 0x3f ) ) .
-                    chr( 0x80 | ( $decimal & 0x3f ) );
-
-            default:
-                throw new \RuntimeException( "Invalid or unhandled codepoint." );
-        }
     }
 }
 
